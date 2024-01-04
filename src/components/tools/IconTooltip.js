7e12b7b07e5a469abc8127cb15a08ab569e1427e
RichardPanigor <richardpanigors02@gmail.com>
2024-01-04 10:08:11 +0700

    ubah icon location

--- src/components/tools/IconTooltip.js
+++ src/components/tools/IconTooltip.js
@@ -1,6 +1,3 @@
-import newMark from '../../assets/image/newmark.svg'
-import newMark2 from '../../assets/image/newmark2.svg'
-
 export const marker = new L.Icon({
     iconUrl: 'https://www.les-voyageuses.net/wp-content/uploads/2017/11/map-marker-coffee.png',
     iconSize: [30, 38],
